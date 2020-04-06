1483256bec1cd6bb8bf6e8eca766ebe9b4175936
anthonysea <anthonyqchao@gmail.com>
2020-04-05 22:21:46 -0700

    changed endpoints to query db instead of loading json file for each request

--- endpoints.py
+++ endpoints.py
@@ -9,27 +9,36 @@ def get_fishes():
     return fish_schema.dump(fish)
 
 def get_fish(fish_id):
-    pass
-    # with open('critters.json') as f:
-    #     data = json.load(f)
-    # fish_query = [fish for fish in data if fish['id'] == fish_id]
-    # if fish_query:
-    #     fish = fish_query[0]
-    # else:
-    #     return {
-    #         'detail': f'The fish with id {fish_id} does not exist',
-    #         'status': 404,
-    #         'title': 'Fish Not Found',
-    #         'type': 'about:blank',
-    #     }
-    # return fish, 200
+    fish = Critter.query.filter(Critter.id==fish_id, Critter.critter_type=='fish').one_or_none()
+
+    if not fish:
+        return {
+            'detail': f'The fish with id {fish_id} does not exist',
+            'status': 404,
+            'title': 'Fish Not Found',
+            'type': 'about:blank',
+        }
+
+    fish_schema = CritterSchema()
+    return fish_schema.dump(fish)
 
 def get_bugs():
-    '''Load all bugs from critters.json'''
+    '''Load all bugs from database'''
     bugs = Critter.query.filter(Critter.critter_type=='bug').all()
 
     bug_schema = CritterSchema(many=True)
     return bug_schema.dump(bugs)
 
 def get_bug(bug_id):
-    pass
+    bug = Critter.query.filter(Critter.id==bug_id, Critter.critter_type=='bug').one_or_none()
+
+    if not bug:
+        return {
+            'detail': f'The bug with id {bug_id} does not exist',
+            'status': 404,
+            'title': 'Bug Not Found',
+            'type': 'about:blank',
+        }
+
+    bug_schema = CritterSchema()
+    return bug_schema.dump(bug)
