5a6afbca848a854bd5a616d0692991a0b3b9673b
anthonysea <anthonyqchao@gmail.com>
2020-04-06 14:22:47 -0700

    parsed fossils page and successfully outputed fossils.json with both stand-alone and multi-part fossils

--- acnh_scraper/spiders/fossil_spider.py
+++ acnh_scraper/spiders/fossil_spider.py
@@ -16,13 +16,33 @@ class FossilSpider(scrapy.Spider):
         sa_rows = sa_fossil_table.css("tr")[1:] 
         mp_rows = mp_fossil_table.css("tr")[1:]
 
-        # build stand-alone fossil list
+        # Build stand-alone fossil and append to fossil list
         for row in sa_rows:
             fossil = {}
             fossil["name"] = row.css("td:first-child > a::text").get()
             fossil["image_url"] = row.css("td:nth-child(2) > a::attr(href)").get()
             fossil["price"] = int("".join(row.css("td:nth-child(3)::text").get().strip().split()[0].split(",")))
             fossil["type"] = "stand-alone"
+            fossil["group"] = None
+
+            fossils.append(fossil)
+
+        # Build multi-part fossils and append to fossil list
+        current_fossil_group = ''
+        for row in mp_rows:
+            fossil = {}
+
+            # Check if current row is a new fossil group
+            if row.css("th > a::text").get():
+                current_fossil_group = row.css("th > a::text").get()
+                continue
+
+            fossil["name"] = row.css("td:first-child > a::text").get()
+            fossil["image_url"] = row.css("td:nth-child(2) > a::attr(href)").get()
+            fossil["price"] = int("".join(row.css("td:nth-child(3)::text").get().strip().split()[0].split(",")))
+            fossil["type"] = "multi-part"
+            fossil["group"] = current_fossil_group
+
             fossils.append(fossil)
 
         return fossils
