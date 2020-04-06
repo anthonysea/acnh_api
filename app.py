363a770f648e6defeb641093eeefad62cdeb1bcf
anthonysea <anthonyqchao@gmail.com>
2020-04-05 17:54:48 -0700

    db properly configured (i think)

--- app.py
+++ app.py
@@ -4,44 +4,30 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
-from scrapy.crawler import CrawlerProcess
-
-from acnh_scraper.spiders.critter_spider import CritterSpider
 from models import db, ma
 
 def create_app():
-    app = connexion.App(__name__, specification_dir='./')
+    connexion_app = connexion.App(__name__, specification_dir='./')
     # Connexion stores the Flask app reference in .app member
-    flask_app = app.app
+    flask_app = connexion_app.app
 
-    flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    print('basedir: ', basedir)
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+    print(flask_app.config['SQLALCHEMY_DATABASE_URI'])
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     flask_app.config['SQLALCHEMY_ECHO'] = True
     CORS(flask_app)
-    app.add_api("api.yml")
+
+    # Use the api.yml for REST API definition
+    connexion_app.add_api("api.yml")
 
     db.init_app(flask_app)
     ma.init_app(flask_app)
 
     return flask_app
 
-def get_data():
-    process = CrawlerProcess(settings={
-        'FEED_FORMAT': 'json',
-        'FEED_URI': 'critters.json'
-    })
-
-    process.crawl(CritterSpider)
-    process.start()
-
 if __name__ == '__main__':
-    json_files = ['critters.json']
-
-    # scrape and create json data files
-    for file in json_files:
-        if not os.path.isfile(file):
-            get_data()
-
     app = create_app()
     
     app.run(debug=True, host='0.0.0.0', port=5000) 
--- build_db.py
+++ build_db.py
@@ -1,10 +1,35 @@
 import os
 from app import create_app
-from models import db
 
+from models import db, Critter, CritterSchema
+from scrapy.crawler import CrawlerProcess
+from acnh_scraper.spiders.critter_spider import CritterSpider
+
+
+def get_data():
+    process = CrawlerProcess(settings={
+        'FEED_FORMAT': 'json',
+        'FEED_URI': 'critters.json'
+    })
+
+    process.crawl(CritterSpider)
+    process.start()
+
+### Start of DB creation
 if os.path.exists('test.db'):
     os.remove('test.db')
 
-db.create_all(app=create_app())
+app = create_app()
+with app.app_context():
+    db.create_all()
+    db.session.commit()
 
+    # Testing
+    # c1 = Critter(name='fish1', price=100, location='river', shadow_size='1')
+    # db.session.add(c1)
+    # db.session.commit()
+
+    critters = Critter.query.all()
+    critter_schema = CritterSchema(many=True)
+    print(critter_schema.dump(critters))
 
